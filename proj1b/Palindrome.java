1b5870af5d7f94694f84f8c243174e675a1431ff
Knyn <2929302213@qq.com>
2023-10-08 13:33:51 +0800

    finish proj1b!

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,42 @@
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> dword = new ArrayDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            dword.addLast(word.charAt(i));
+        }
+        return dword;
+    }
+
+    public boolean isPalindrome(String word) {
+        Deque<Character> dword = wordToDeque(word);
+
+        /**
+        while(Dword.size() > 1) {
+            if(Dword.removeFirst() != Dword.removeLast())
+                return false;
+        }
+        return true;    */
+
+        return isPalindromeHelper(dword);
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        Deque<Character> dword = wordToDeque(word);
+
+        while (dword.size() > 1) {
+            if (!cc.equalChars(dword.removeFirst(), dword.removeLast())) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    private boolean isPalindromeHelper(Deque<Character> word) {
+        if (word.isEmpty() || word.size() == 1) {
+            return true;
+        } else {
+            return (word.removeFirst() == word.removeLast()) && isPalindromeHelper(word);
+        }
+    }
+}
